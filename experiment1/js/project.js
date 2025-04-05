c9da6d0f60e259b1fba903d549a7970444da06c2
shawn-sw <swang387@ucsc.edu>
2025-04-04 20:35:03 -0700

    creates a text generator

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,59 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    adventurer: ["Trainer", "Rookie", "Champion", "Challenger"],
+    message: ["message", "alert", "signal", "announcement"],
+    pre: [
+      "Cowell College",
+      "Stevenson College",
+      "Merrill College",
+      "Kresge College",
+      "Oakes College",
+    ],
+    post: ["lab", "rooftop", "basement", "classroom"],
+    baddies: ["Team Rocket", "Team Galactic", "Team Plasma", "Team Flare"],
+    monster: ["Gengar", "Ursaring", "Tyranitar", "Obstagoon"],
+    pokemon: ["Arcanine", "Dragonite", "Metagross", "Lucario", "Flygon"],
+    num: ["3", "5", "8", "a rare number of", "many"],
+    loots: ["Poké Ball", "Potion", "Coin"],
+  };
+  const template = `$adventurer, please pay attention to this $message!
+  
+  $pre’s $post is in grave danger — it has been taken over by $baddies, who have sent $monster to wreak havoc. You must head out immediately and stop them — take your Pokémon $pokemon, with you!
+  
+  The one who saves them will be rewarded with $num $loots. That’s it — go forth!`;
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    //box.innerText = story;
+    $("#box").html(story.replace(/\n/g, "<br>"));
+  }
+  
+  /* global clicker */
+  //clicker.onclick = generate;
+  $("#clicker").click(generate);
+  
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
